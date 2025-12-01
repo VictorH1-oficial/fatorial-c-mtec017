170630dbf1f3133919661df414ad4cd7e8735ed0
VictorH1-oficial <biaribs06@gmail.com>
2025-12-01 20:25:50 -0300

    Fix syntax errors in fatorial_test.c

--- fatorial_test.c
+++ fatorial_test.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+
 #include <assert.h>
 #include "fatorial.c"
 
 int main() {
-assert(fatorial(5)==120));
-assert(fatorial(0)==1));
+assert(fatorial(5)==120);
+assert(fatorial(0)==1);
 assert(fatorial(-3)==-1;
     return 0;
 }
