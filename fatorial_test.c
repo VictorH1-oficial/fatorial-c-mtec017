a11da430bf2e9898780adf94e87e0def6f3de0bb
VictorH1-oficial <biaribs06@gmail.com>
2025-12-01 20:22:22 -0300

    Refactor factorial tests and fix assert statement
    Removed redundant test output and fixed assert syntax.

--- fatorial_test.c
+++ fatorial_test.c
@@ -5,16 +5,6 @@
 int main() {
 assert(fatorial(5)==120));
 assert(fatorial(0)==1));
-    
-
-    
-    if (fatorial(-3) == -1)
-        printf("Teste 3 OK\n");
-    else {
-        printf("Teste 3 FALHOU\n");
-        return 1;
-    }
-
-    printf("Todos os testes passaram!\n");
+assert(fatorial(-3)==-1;
     return 0;
 }
