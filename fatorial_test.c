253d00cd0677781c0fa51a76fa4ab70dd0348db9
VictorH1-oficial <biaribs06@gmail.com>
2025-12-01 20:27:28 -0300

    Fix syntax error in fatorial_test.c

--- fatorial_test.c
+++ fatorial_test.c
@@ -1,10 +1,9 @@
-
 #include <assert.h>
 #include "fatorial.c"
 
 int main() {
 assert(fatorial(5)==120);
 assert(fatorial(0)==1);
-assert(fatorial(-3)==-1;
+assert(fatorial(-3)==-1);
     return 0;
 }
